81630ef0e16b53750a688ec4262c917cc167002d
Wouter de Wild <wouter@codewild.nl>
2023-12-06 00:05:52 +0100

    I dont even care

---
+++ packages/day-5/src/main.rs
@@ -0,0 +1,89 @@
+use std::collections::HashMap;
+
+fn main() {
+    let input = include_str!("input")
+        .splitn(2, "\n\n")
+        .collect::<Vec<&str>>();
+
+    let seeds: Vec<i64> = input.get(0).unwrap()[7..]
+        .split_whitespace()
+        .map(|s| s.parse::<i64>().unwrap())
+        .collect();
+
+    let maps: HashMap<(String, String), Vec<(i64, i64, i64)>> =
+        input.get(1).unwrap().split("\n\n").fold(
+            HashMap::new(),
+            |mut maps: HashMap<(String, String), Vec<(i64, i64, i64)>>, c| {
+                let half = c.split(" map:\n").collect::<Vec<&str>>();
+
+                let source_dest = half.get(0).unwrap().split("-to-").collect::<Vec<&str>>();
+
+                maps.insert(
+                    (
+                        source_dest.get(0).unwrap().to_string(),
+                        source_dest.get(1).unwrap().to_string(),
+                    ),
+                    half.get(1)
+                        .unwrap()
+                        .lines()
+                        .map(|line| {
+                            let items = line
+                                .split_whitespace()
+                                .map(|v| v.parse::<i64>().unwrap())
+                                .collect::<Vec<i64>>();
+
+                            (
+                                *items.get(0).unwrap(),
+                                *items.get(1).unwrap(),
+                                *items.get(2).unwrap(),
+                            )
+                        })
+                        .collect::<Vec<(i64, i64, i64)>>(),
+                );
+
+                maps
+            },
+        );
+
+    println!("Day five");
+    println!(
+        "Part one: {}",
+        seeds
+            .iter()
+            .map(|seed| go_to_location("seed", seed, &maps))
+            .min()
+            .unwrap()
+    );
+}
+
+fn go_to_location(
+    target: &str,
+    number: &i64,
+    maps: &HashMap<(String, String), Vec<(i64, i64, i64)>>,
+) -> i64 {
+    let ((_, destination), ranges) = maps
+        .iter()
+        .find(|((source, _), _)| source == target)
+        .unwrap();
+
+    let next_number = ranges
+        .iter()
+        .filter_map(|(destination, source, range)| {
+            if number >= source && number <= &(source + range) {
+                return Some(number - source + destination);
+            }
+
+            None
+        })
+        .collect::<Vec<i64>>();
+
+    if target != "humidity" {
+        return go_to_location(
+            destination,
+            next_number.get(0).unwrap_or_else(|| number),
+            maps,
+        );
+    }
+
+    *next_number.get(0).unwrap_or_else(|| number)
+}
